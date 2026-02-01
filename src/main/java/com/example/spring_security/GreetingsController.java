30ea78fa080282d1f671a53a295b689c1663158e
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-01 23:55:23 +0700

    Testing Security

---
+++ src/main/java/com/example/spring_security/GreetingsController.java
@@ -0,0 +1,12 @@
+package com.example.spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/example/spring_security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.spring_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests((requests) -> (requests.anyRequest()).authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}

ba4c04d584309af9416aab3dd50f99066c653275
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-02 14:43:05 +0700

    Using userDetailsService

--- src/main/java/com/example/spring_security/SecurityConfig.java
+++ src/main/java/com/example/spring_security/SecurityConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +18,22 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
         http.authorizeHttpRequests((requests) -> (requests.anyRequest()).authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}userpassword")
+                .roles("USER")
+                .build();
+
+        UserDetails admin1 = User.withUsername("admin1")
+                .password("{noop}adminpassword")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, admin1);
     }
 }

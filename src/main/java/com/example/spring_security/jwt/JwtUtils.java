7aa1694ee40898e6461c9b793cbd0b6f57f042ff
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-03 23:35:41 +0700

    Add key and username generation from token

--- src/main/java/com/example/spring_security/jwt/JwtUtils.java
+++ src/main/java/com/example/spring_security/jwt/JwtUtils.java
@@ -1,12 +1,15 @@
 package com.example.spring_security.jwt;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
@@ -15,6 +18,8 @@ public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
     private int jwtExpirationMs;
+
+    private String jwtSecret;
 
     public String getJwtFromHeader(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
@@ -32,5 +37,18 @@ public class JwtUtils {
                 .expiration(new Date(new Date().getTime() + jwtExpirationMs))
                 .signWith(key())
                 .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parser()
+                .verifyWith(key())
+                .build().parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
+
+    public SecretKey key() {
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
     }
 }

33f2fdce46473e04a02d60b884070d0a366aa05d
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-04 23:57:23 +0700

    Update AuthEntryPointJwt

--- src/main/java/com/example/spring_security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/example/spring_security/jwt/AuthEntryPointJwt.java
@@ -8,12 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
